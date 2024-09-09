7bcef7d7cbaa53172c8e42a55b6004afd867243d
Yangyya9 <202230310097@stu.shmtu.edu.cn>
2024-09-09 20:01:54 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -50,6 +50,39 @@ class cifar_100():
         x3 = self.V3[idx]
         return [torch.from_numpy(x1), torch.from_numpy(x2), torch.from_numpy(x3)], self.Y[idx], torch.from_numpy(np.array(idx)).long()
 
+class YoutubeFace_sel_fea():
+    def __init__(self, path):
+        data = scipy.io.loadmat(path + 'YoutubeFace_sel_fea.mat')
+        self.Y = data['Y'].astype(np.int32).reshape(101499,)
+        self.V1 = data['X'][0][0].astype(np.float32)
+        self.V2 = data['X'][1][0].astype(np.float32)
+        self.V3 = data['X'][2][0].astype(np.float32)
+        self.V4 = data['X'][3][0].astype(np.float32)
+        self.V5 = data['X'][4][0].astype(np.float32)
+    def __len__(self):
+        return 101499
+    def __getitem__(self, idx):
+        x1 = self.V1[idx]
+        x2 = self.V2[idx]
+        x3 = self.V3[idx]
+        x4 = self.V4[idx]
+        x5 = self.V5[idx]
+        return [torch.from_numpy(x1), torch.from_numpy(x2), torch.from_numpy(x3), torch.from_numpy(x4), torch.from_numpy(x5)], self.Y[idx], torch.from_numpy(np.array(idx)).long()
+
+class synthetic3d():
+    def __init__(self, path):
+        data = scipy.io.loadmat(path + 'synthetic3d.mat')
+        self.Y = data['Y'].astype(np.int32).reshape(600,)
+        self.V1 = data['X'][0][0].astype(np.float32)
+        self.V2 = data['X'][1][0].astype(np.float32)
+        self.V3 = data['X'][2][0].astype(np.float32)
+    def __len__(self):
+        return 600
+    def __getitem__(self, idx):
+        x1 = self.V1[idx]
+        x2 = self.V2[idx]
+        x3 = self.V3[idx]
+        return [torch.from_numpy(x1), torch.from_numpy(x2), torch.from_numpy(x3)], self.Y[idx], torch.from_numpy(np.array(idx)).long()
 
 def load_data(dataset):
     if dataset == "CCV":
@@ -70,6 +103,18 @@ def load_data(dataset):
         view = 3
         data_size = 50000
         class_num = 100
+    elif dataset == "YouTubeFace":
+        dataset = YoutubeFace_sel_fea('./data/')
+        dims = [64, 512, 64, 647, 838]
+        view = 5
+        data_size = 101499
+        class_num = 31
+    elif dataset == 'Synthetic3d':
+        dataset = synthetic3d('./data/')
+        dims = [3, 3, 3]
+        view = 3
+        data_size = 600
+        class_num = 3
     else:
         raise NotImplementedError
     return dataset, dims, view, data_size, class_num
